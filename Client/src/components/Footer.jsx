b5cab5646ffff72028f9acf37dc3f3eb8af996a8
yonas pro <yonasbe999@gmail.com>
2025-02-14 11:14:55 -0900

    Front end First Code

---
+++ Client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <div className='container mx-auto p-4'>
+            <p>All Rights Reserved.</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <header className="h-20 shadow-md">Header</header>;
+}
+
+export default Header;
