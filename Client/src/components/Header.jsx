e9846493cabe21da0e029d6c52899fc582d429b9
yonas pro <yonasbe999@gmail.com>
2025-02-15 20:59:50 -0900

    Frontend second code

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,29 +1,61 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import Search from "./Search";
+import { Link, useLocation } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
+import useMobile from "../hooks/useMobile";
+
 
 function Header() {
+  const [isMobile] = useMobile()
+
+  const location = useLocation()
+
+  console.log("location", location)
+  console.log("ismobile", isMobile)
   return (
-    <header className="h-20 shadow-md sticky top-0">
-      <div className="container flex mx-auto items-center h-full px-2 justify-between">
+    <header className="h-24 lg:h-20 lg:shadow-md sticky top-0 bg-red-400 flex flex-col justify-center gap-2">
+      <div className="container lg:h-full flex mx-auto items-center  px-2 justify-between">
         {/* logo */}
         <div className="h-full">
-          <div className="h-full justify-center flex items-center px-2 ">
+          <Link
+            to={"/"}
+            className="h-full justify-center flex items-center px-2 "
+          >
             {/* logo for desktop version  */}
-            <img src={logo} alt="Logo" width={170} height={60} className="hidden lg:block"/>
-             {/* logo for Mobile version  */}
-            <img src={logo} alt="Logo" width={120} height={60} className="lg:hidden"/>
-          </div>
+            <img
+              src={logo}
+              alt="Logo"
+              width={170}
+              height={60}
+              className="hidden lg:block"
+            />
+            {/* logo for Mobile version  */}
+            <img
+              src={logo}
+              alt="Logo"
+              width={120}
+              height={60}
+              className="lg:hidden"
+            />
+          </Link>
         </div>
         {/* Search */}
-        <div>
-          <Search/>
+        <div className="hidden lg:block">
+          <Search />
         </div>
 
         {/* login and My cart */}
-        <div>
-          Login And my Cart
+
+        <div className="">
+          <button className="text-neutral-600 lg:hidden ">
+            <FaUser size={26}/>
+          </button>
+        <div className="hidden lg:block">Login And my Cart</div>
         </div>
+      </div>
+      <div className="container mx-auto px-2 lg:hidden">
+        <Search/>
       </div>
     </header>
   );
--- Client/src/components/Search.jsx
+++ Client/src/components/Search.jsx
@@ -1,32 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import { TypeAnimation } from 'react-type-animation';
+import { useLocation, useNavigate } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
 function Search() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [isSearchPage, setIsSearchPage] = useState(false);
+
+  useEffect(() => {
+    const isSearch = location.pathname === "/search";
+    setIsSearchPage(isSearch);
+  }, [location]);
+
+  const redirectToSearchPage = () => {
+    navigate("/search");
+  };
   return (
-    <div className='w-full min-w-[300px] lg:min-w-[420px] h-12 rounded-lg border overflow:hidden flex items-center text-neutral-500 bg-slate-50' >
-        <button className='flex justify-center items-center h-full p-3 '>
-        <IoSearch size={20}/>
-        </button>
-        <div>
-        <TypeAnimation
-      sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'Search "milk"',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'Search "bread"',
-        1000,
-        'Search "sugar"',
-        1000,
-        'Search "melon"',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      repeat={Infinity}
-    />
-        </div>
+    <div className="w-full min-w-[300px] lg:min-w-[420px]   lg:h-12 h-10 rounded-lg border overflow:hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200">
+      <button className="flex justify-center items-center h-full p-3   group focus-within:text-primary-200">
+        <IoSearch size={22} />
+      </button>
+      <div className="w-full h-full">
+        {!isSearchPage ? (
+          //  not in search  page
+          <div onClick={redirectToSearchPage} className="w-full h-full flex items-center">
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Search "milk"',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Search "bread"',
+                1000,
+                'Search "sugar"',
+                1000,
+                'Search "melon"',
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </div>
+        ) : (
+          //  when I was Search page
+          <div className="w-full h-full">
+            <input type="text" placeholder="Search for Ata dal and more" className="bg-transparent h-full w-full outline-none" autoFocus />
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
